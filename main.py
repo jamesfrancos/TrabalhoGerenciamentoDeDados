042bde8b84a7fff212816da8054b0213bff964ef
37charles <37charlessilva@gmail.com>
2025-12-04 11:51:29 -0300

    media de publico

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ df_spark = load_data()
 df = toPandas()
 dfToFilter = df
 
+# sidebar - Filtros
 anos = ["Todos"] + sorted(df["ano_campeonato"].unique())
 ano = st.sidebar.selectbox("Selecione o Ano",anos)
 
@@ -56,6 +57,7 @@ media_gols = (
     .sort_values()
 )
 
+
 # Escolhendo o Ano
 
 
@@ -67,7 +69,7 @@ media_gols = (
 
 # 1) Se NÃO escolher time
 if time == "Todos":
-
+    # media de gols de todos os times
     st.subheader(f"Média de gols por time {opcao}")
 
     fig, ax = plt.subplots(figsize=(12, 6))
@@ -79,6 +81,18 @@ if time == "Todos":
 
     st.pyplot(fig)
 
+    # Média de publico
+    if ano == "Todos":
+        publico_medio = dfToFilter.groupby(tipo[0])["publico"].mean().sort_values()
+        fig1, ax1 = plt.subplots(figsize=(12, 6))
+        st.subheader(f"Média de publico por time {opcao}")
+        ax1.bar(publico_medio.index, publico_medio.values)
+        ax1.set_xticklabels(publico_medio.index, rotation=90)
+        ax1.set_title(f"Público médio por time {opcao}")
+        ax1.set_xlabel("Time")
+        ax1.set_ylabel("Público médio")
+    
+        st.pyplot(fig1)
 # 2) Se escolher um time
 else:
     st.subheader(f"Média de gols do {time} como {opcao} por ano")
@@ -98,3 +112,38 @@ else:
     ax.set_ylabel("Média de gols")
 
     st.pyplot(fig)
+
+    if ano == "Todos":
+        # Média de publico por ano
+        publico_medio_ano = (
+        dfToFilter[dfToFilter[tipo[0]] == time]
+        .groupby("ano_campeonato")["publico"]
+        .mean()
+        .sort_index()
+    )
+        texto = f"Média de publico por ano {opcao}"
+    else:
+        # Média de publico por rodada
+        publico_medio_ano = (
+            dfToFilter[dfToFilter[tipo[0]] == time]
+            .groupby("rodada")["publico"]
+            .mean()
+            .sort_index()
+        )   
+        texto = f"Média de publico por rodada {opcao}"
+
+    print(publico_medio_ano)
+
+    fig1, ax1 = plt.subplots(figsize=(12, 6))
+    # fazer um if pra trocar por rodada tambem
+    st.subheader(texto)
+
+    ax1.plot(publico_medio_ano.index.astype(int), publico_medio_ano.values, marker="o")
+    #ax1.set_xticklabels(publico_medio_ano.index, rotation=90)
+    ax1.set_xticks(publico_medio_ano.index.astype(int))
+    ax1.set_xticklabels(publico_medio_ano.index.astype(int), rotation=45)
+
+    ax1.set_title(f"ano")
+    ax1.set_xlabel("Time")
+    ax1.set_ylabel("Público médio")
+    st.pyplot(fig1)
